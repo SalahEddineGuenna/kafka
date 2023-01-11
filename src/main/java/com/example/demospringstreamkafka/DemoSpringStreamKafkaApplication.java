72d0028c8f8de6898343e7343b6bcb734f5ce3ec
salah eddine <salimali1768@gmail.com>
2023-01-11 19:46:07 +0100

    kafka

---
+++ src/main/java/com/example/demospringstreamkafka/DemoSpringStreamKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringstreamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringStreamKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringStreamKafkaApplication.class, args);
+    }
+
+}
